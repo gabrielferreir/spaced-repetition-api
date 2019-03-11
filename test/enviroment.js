f15de4117bbb0ef0beb813321588999d43f9841c
Gabriel Ferreira <gabrielferrer@outlook.com.br>
2019-03-11 10:15:43 -0300

    TDD with chai-http

---
+++ test/enviroment.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: 'http://localhost:3002',
+    authentication: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdhYnJpZWwuZmVycmVpcmFAc21uLmNvbS5iciIsInBhc3MiOiJ0ZXN0ZTEyMyIsImlkIjoiNWMzNjg0MDljZDM3ODQyM2Q4OGU5NWNlIiwiaWF0IjoxNTUyMTcwNjYxfQ.9RnqymnrRJbmgTd44_DxvoyJCPh4tQqEEuU-TpwBorA'
+};
--- test/flashcards.js
+++ test/flashcards.js
@@ -1,5 +1,11 @@
 const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const enviroment = require('./enviroment');
 const {refeshBox, nextDateRevison} = require('../services/flashcards');
+
+chai.use(chaiHttp);
 
 const FLASHCARDS = {
     'box': 4,
@@ -25,5 +31,18 @@ describe('Flashcard Test', () => {
         const nextDate = nextDateRevison(currentDate, currentBox);
         assert.deepEqual(dateRevison, nextDate);
     })
+
+});
+
+describe('Flashcard API Test', () => {
+
+    it('should GET flashcards', () => {
+        chai.request(enviroment.url)
+            .get('/flashcards')
+            .set('authentication', enviroment.authentication)
+            .end((err, res) => {
+                res.should.have.status(200);
+            })
+    });
 
 });
